275995b86603186c464ca381dca5c54ca3fba046
Anas <anas.alhariri@study.hs-duesseldorf.de>
2023-10-23 19:13:55 +0200

    Erzaehle mir einen Witz

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -13,4 +13,10 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
+    [r"[Ee]rzaehle mir (?:einen|einen weiteren) Witz",
+    ["Natürlich! Hier ist ein Witz für dich: Warum hat der Kühlschrank den Laptop nicht bezahlt? Weil er schon eine eigene Rechnung hatte!",
+     "Klar, hier ist einer: Was macht ein Clown im Büro? Faxen!",
+     "Warum hat der Mathematiker eine Brille? Weil er mit Zahlen jongliert!",
+     "Was ist grün und läuft durch den Wald? Eine Rudel Gurken!"]],
+
 ]
