909218bb2a78b7df9668c512e85c321868e39070
Pepe G. Krumminga <pepe.krumminga@study.hs-duesseldorf.de>
2023-10-26 10:26:30 +0200

    Added second text pattern to Psychobabble

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -12,5 +12,15 @@ psychobabble = [
     ["Warum brauchst du {0}?",
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
+    
+    [r"Was ist (.*)",
+    ["Ist {0} nicht hochkomplex und kann nicht in wenigen Worten beschrieben werden.",
+    "Wenn du {0} lang genug betrachtest,\nwird sich eine neue Erkenntnis kristallisieren.",
+    "Ich kann {0} nur empfehlen."]],
+    
+    [r"Was (kann|soll) .*",
+     ["Die Entscheidung liegt letztenendes bei dir selber. Hör auf dein Herz und es wird dich leiten.",
+      "Du {0}st deine Gedanken noch einmal erforschen um einen neuen Pfad zu finden.",
+      "Niemand {0} dir besser helfen als du selbst."]]
 
 ]
